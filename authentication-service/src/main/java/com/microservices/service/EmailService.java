cd59fa84e66fa8b2b5220520c68e7b3a05b1d280
amira.taha <amira.taha@asset.com.eg>
2024-12-21 16:25:06 +0200

    backend code merged

--- authentication-service/src/main/java/com/microservices/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.microservices.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface EmailService {
-    boolean sendActivationEmail(String userEmail, String activationToken);
-    boolean sendResetPasswordEmail(String email, String token);
-}
